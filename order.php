0946fcd93bfcee805031eb5b3ee4b118e36fc8e1
mxzik <ximachk@mail.ru>
2019-12-08 17:47:51 +0300

    5

--- order.php
+++ order.php
@@ -1,33 +1,54 @@
 <?php
-$lastname = $_POST['lastname'];
-$firstname = $_POST['firstname'];
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
 $email = $_POST['email'];
-$country = $_POST['email'];
-$comment = $_POST['comment'];
-$confirm = $_POST['confirm'];
-$lastname = htmlspecialchars($lastname);
-$firstname = htmlspecialchars($firstname);
-$email = htmlspecialchars($email);
-$country = htmlspecialchars($country);
-$comment = htmlspecialchars($comment);
-$confirm = htmlspecialchars($confirm);
-$lastname = urldecode($lastname);
-$firstname = urldecode($firstname);
-$email = urldecode($email);
-$country = urldecode($country);
-$comment = urldecode($comment);
-$confirm = urldecode($confirm);
-$lastname = trim($lastname);
-$firstname = trim($firstname);
-$email = trim($email);
-$country = trim($country);
-$comment = trim($comment);
-$confirm = trim($confirm);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("puderasts@mail.ru", "Отзыв", "Фамилия:".$lastname.".Имя: ".$firstname ". E-mail: ".$email. ".Страна: ".$country. ".Отзыв: ".$comment. ".Подтверждение на использование: ".$confirm , "From: ximachk@mail.ru \r\n"))
- {     echo "сообщение успешно отправлено";
+$text = $_POST['text'];
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $msg = "ok";
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";                                          
+    $mail->SMTPAuth   = true;
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.ru'; // SMTP сервера GMAIL
+    $mail->Username   = 'Kiril Himach'; // Логин на почте
+    $mail->Password   = '2904200154956kx'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('mxzikz8@gmail.com', 'Kiril Himach'); // Адрес самой почты и имя отправителя
+    // Получатель письма
+    $mail->addAddress('mxzikz7@gmail.com');  
+    // Прикрипление файлов к письму
+if (!empty($_FILES['myfile']['name'][0])) {
+    for ($ct = 0; $ct < count($_FILES['myfile']['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['myfile']['name'][$ct]));
+        $filename = $_FILES['myfile']['name'][$ct];
+        if (move_uploaded_file($_FILES['myfile']['tmp_name'][$ct], $uploadfile)) {
+            $mail->addAttachment($uploadfile, $filename);
+        } else {
+            $msg .= 'Неудалось прикрепить файл ' . $uploadfile;
+        }
+    }   
+}
+        // -----------------------
+        // Само письмо
+        // -----------------------
+        $mail->isHTML(true);
+    
+        $mail->Subject = 'Заголовок письма';
+        $mail->Body    = "<b>Имя:</b> $name <br>
+        <b>Почта:</b> $email<br><br>
+        <b>Сообщение:</b><br>$text";
+// Проверяем отравленность сообщения
+if ($mail->send()) {
+    echo "$msg";
 } else {
-    echo "при отправке сообщения возникли ошибки";
-}?>
+echo "Сообщение не было отправлено. Неверно указаны настройки вашей почты";
+}
+} catch (Exception $e) {
+    echo "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
